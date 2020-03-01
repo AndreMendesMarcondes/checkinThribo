6a505e951aeb1f48b82356b14725309ed2489664
André Mendes Marcondes <andremendesmarcondes@gmail.com>
2020-03-01 20:45:38 -0300

    Inclusao projeto

---
+++ src/main/java/com/mineiro/checkInThribo/CheckInThriboApplication.java
@@ -0,0 +1,13 @@
+package com.mineiro.checkInThribo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInThriboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckInThriboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mineiro/checkInThribo/CheckInThriboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mineiro.checkInThribo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInThriboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
